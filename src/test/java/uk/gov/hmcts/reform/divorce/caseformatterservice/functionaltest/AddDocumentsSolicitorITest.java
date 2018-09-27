3cb98cbf33179d9f172c11aeb2d6d0845bca9b2c
tony.chow <tony.chow@hmcts.net>
2018-09-27 12:28:24 +0100

    Unit test for Solicitor model details

---
+++ src/test/java/uk/gov/hmcts/reform/divorce/caseformatterservice/functionaltest/AddDocumentsSolicitorITest.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.reform.divorce.caseformatterservice.functionaltest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.divorce.caseformatterservice.CaseFormatterServiceApplication;
+import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.ccd.CoreCaseData;
+import uk.gov.hmcts.reform.divorce.caseformatterservice.mapper.ObjectMapperTestUtil;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = CaseFormatterServiceApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource(value = "classpath:application.yml")
+@AutoConfigureMockMvc
+public class AddDocumentsSolicitorITest {
+    private static final String API_URL = "/caseformatter/version/1/add-documents";
+    private static final String PAYLOAD_PATH = "fixtures/model/ccd/solicitorCaseDocumentsInput.json";
+    private static final String EXPECTED_PAYLOAD_PATH = "fixtures/model/ccd/solicitorCaseDocumentsOutput.json";
+
+    @Autowired
+    private MockMvc webClient;
+
+    @Test
+    public void givenValidSolicitorDetails_whenAddDocuments_thenReturnExpected() throws Exception {
+        final CoreCaseData expectedCaseData =
+            (CoreCaseData)ObjectMapperTestUtil.jsonToObject(EXPECTED_PAYLOAD_PATH, CoreCaseData.class);
+
+        MvcResult result = webClient.perform(post(API_URL)
+            .content(ObjectMapperTestUtil.loadJson(PAYLOAD_PATH))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        final CoreCaseData actualCaseData =
+            (CoreCaseData)ObjectMapperTestUtil.jsonStringToObject(result.getResponse().getContentAsString(),
+                CoreCaseData.class);
+
+        assertThat(expectedCaseData, samePropertyValuesAs(actualCaseData));
+    }
+}

5bfa14e754349dc7888cb932c61199145701e6c5
tony.chow <tony.chow@hmcts.net>
2019-02-05 11:00:05 +0000

    Replace exisitngPayment properties that are not null only

--- src/test/java/uk/gov/hmcts/reform/divorce/caseformatterservice/strategy/payment/ExistingPaymentReferenceStrategyUTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/caseformatterservice/strategy/payment/ExistingPaymentReferenceStrategyUTest.java
@@ -34,10 +34,31 @@ public class ExistingPaymentReferenceStrategyUTest {
         assertThat(returnedPaymentsList, equalTo(expectedPaymentsList));
     }
 
+    @Test
+    public void testExistingPaymentReferenceAndSubsetOfPaymentWillUpdatePayment() {
+        final PaymentCollection newPayment = createPayment("111222333", null, "123456789");
+        final PaymentCollection existingPayment = createPayment("999888777", "success", "12345");
+        final PaymentCollection toBeReplacedPayment = createPayment("111222333", "created", "123");
+
+        final List<PaymentCollection> existingPaymentsList = new ArrayList<>();
+        existingPaymentsList.add(existingPayment);
+        existingPaymentsList.add(toBeReplacedPayment);
+
+        final PaymentCollection updatedNewPayment = createPayment("111222333", "created", "123");
+
+        final List<PaymentCollection> expectedPaymentsList = asList(existingPayment, updatedNewPayment);
+        final List<PaymentCollection> returnedPaymentsList = existingPaymentReferenceStrategy
+            .getCurrentPaymentsList(newPayment.getValue(), existingPaymentsList);
+
+        assertThat(returnedPaymentsList, equalTo(expectedPaymentsList));
+    }
+
+
     private PaymentCollection createPayment(String reference, String status, String collectionId) {
-        final Payment payment = new Payment();
-        payment.setPaymentReference(reference);
-        payment.setPaymentStatus(status);
+        final Payment payment = Payment.builder()
+            .paymentReference(reference)
+            .paymentStatus(status)
+            .build();
 
         return PaymentCollection.builder().id(collectionId).value(payment).build();
     }

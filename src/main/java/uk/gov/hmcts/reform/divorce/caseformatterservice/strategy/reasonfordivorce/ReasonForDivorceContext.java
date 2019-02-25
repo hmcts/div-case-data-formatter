cb6a0cddeb14f0097cfc60bd01f96b9a3e3b3375
Hosain <hosain.alahmad@hmcts.net>
2019-02-25 14:57:06 +0000

    DIV-4303 - use Autowired for SeparationStrategy in ReasonForDivorceContext

--- src/main/java/uk/gov/hmcts/reform/divorce/caseformatterservice/strategy/reasonfordivorce/ReasonForDivorceContext.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/caseformatterservice/strategy/reasonfordivorce/ReasonForDivorceContext.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.caseformatterservice.strategy.reasonfordivorce;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.ccd.CoreCaseData;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.usersession.DivorceSession;
@@ -11,14 +12,18 @@ import java.util.stream.Collectors;
 @Component
 public class ReasonForDivorceContext {
 
-    private final List<ReasonForDivorceStrategy> reasonForDivorceStrategies =
-        Arrays.asList(
+    private final List<ReasonForDivorceStrategy> reasonForDivorceStrategies;
+
+    @Autowired
+    public ReasonForDivorceContext(SeparationStrategy separationStrategy) {
+        reasonForDivorceStrategies = Arrays.asList(
             new AdulteryStrategy(),
             new DesertionStrategy(),
-            new SeparationFiveYearsStrategy(new SeparationStrategy()),
-            new SeparationTwoYearsStrategy(new SeparationStrategy()),
+            new SeparationFiveYearsStrategy(separationStrategy),
+            new SeparationTwoYearsStrategy(separationStrategy),
             new UnreasonableBehaviourStrategy()
         );
+    }
 
     public String deriveStatementOfWork(DivorceSession divorceSession) {
         return reasonForDivorceStrategies.stream()

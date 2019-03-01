de879f3687c9a24994942cf0129d40ffd75cbbf3
tony.chow <tony.chow@hmcts.net>
2019-03-01 12:41:47 +0000

    Use dynamic document link generation for integration test
    Add document management store url to outputs
    
    Refactor code for cleaniness

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -5,3 +5,7 @@ output "test_environment" {
 output "auth_idam_client_baseUrl" {
     value = "${var.idam_api_baseurl}"
 }
+
+output "document_management_store_url" {
+    value = "${local.dm_store_url}"
+}

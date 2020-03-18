b880980b7f41b3b329aeb20ef8dfc1d3858654af
Rishi Sharma <rishi.sharma@hmcts.net>
2020-03-18 16:41:19 +0000

    Removed unused import- cause build failure

--- src/main/java/uk/gov/hmcts/reform/divorce/caseformatterservice/mapper/DivorceCaseToAosCaseMapper.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/caseformatterservice/mapper/DivorceCaseToAosCaseMapper.java
@@ -7,7 +7,6 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.ccd.AosCaseData;
-import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.ccd.CoreCaseData;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.usersession.DivorceSession;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.usersession.YesNoNeverAnswer;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.domain.model.usersession.corespondent.CoRespondentAnswers;

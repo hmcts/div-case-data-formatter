701e3c0a4d36bbe5d3ac8154f9a85a98906de276
Johnny Vineall <vineall@live.co.uk>
2022-04-13 12:05:31 +0100

    Update app key

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,6 +37,11 @@ module "div-cfs" {
     }
 }
 
+data "azurerm_key_vault" "div_key_vault" {
+    name                = local.vaultName
+    resource_group_name = local.vaultName
+}
+
 data "azurerm_key_vault_secret" "appinsights_secret" {
   name = "AppInsightsInstrumentationKey"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id

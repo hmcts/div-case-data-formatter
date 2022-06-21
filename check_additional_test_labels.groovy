20d706184eae963f04bd7ea27449595672fe7016
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-21 17:44:58 +0100

    Refactor & make pattern/enableAll label configurable

--- check_additional_test_labels.groovy
+++ check_additional_test_labels.groovy
@@ -1,17 +1,19 @@
 #!groovy
 import uk.gov.hmcts.contino.GithubAPI
 
-class AdditionalTestLabels {
-    private Labels = []
-    private LabelStates = [:]
-    private AtLeastOneEnabled = false
+class LabelStates {
+    private ArrayList Labels = []
+    private LinkedHashMap LabelStates = [:]
+    private Boolean AtLeastOneEnabled = false
+    private String Pattern = ""
+    private String EnableAll = ""
 
     private void setLabelStates() {
         def states = [:]
         // Get labels from GitHub repo
-        def repoLabels = new GithubAPI(this).getLabelsbyPattern(env.BRANCH_NAME, "enable_")
+        def repoLabels = new GithubAPI(this).getLabelsbyPattern(env.BRANCH_NAME, this.Pattern)
 
-        // Check existence of provided label, and store in states map
+        // Check existence of provided label, and store result in states map
         def getState = { label ->
             if (label == "atLeastOneEnabled") {
                 return
@@ -23,25 +25,33 @@ class AdditionalTestLabels {
         // Iterate over labels List to build LabelStates map
         this.Labels.each { label -> getState(label) }
 
-        // Hardcode enable_all_tests_and_scans label
-        if (!states.containsKey("enable_all_tests_and_scans")) {
-            getState("enable_all_tests_and_scans")
+        // Set state of EnableAll label )If a label matching this string exists, all label tests will return True)
+        if (!states.containsKey(this.EnableAll)) {
+            getState(this.EnableAll)
         }
 
         this.LabelStates = states
     }
 
+    Boolean isAllEnabled() {
+        return (this.LabelStates[this.EnableAll])
+    }
+    
     Boolean getLabelState(label) {
-        return (this.LabelStates.enable_all_tests_and_scans || this.LabelStates.get(label))
+        return (this.isAllEnabled() || this.LabelStates.get(label))
     }
 
     Boolean isAtLeastOneEnabled() {
         return this.AtLeastOneEnabled
     }
 
-    AdditionalTestLabels(final ArrayList labels) {
+    LabelStates(final ArrayList labels, final String pattern = "enable_", final String enableAllLabel = "enable_all_tests_and_scans") {
         // Define labels
         this.Labels = labels
+        // Define pattern
+        this.Pattern = pattern
+        // Define EnableAll label name (If a label matching this string exists, all label tests will return True)
+        this.EnableAll = enableAllLabel
         // Define label states
         this.setLabelStates()
         // Check if at least one label state is true

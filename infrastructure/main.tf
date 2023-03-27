a811c57aefd73bef593c3c7c6d2dc6742e3ba08b
damongreen123 <damon.green@btinternet.com>
2023-03-27 15:26:18 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 data "azurerm_key_vault" "div_key_vault" {
     name                = "${var.product}-${var.env}"
     resource_group_name = "${var.product}-${var.env}"
-    common_tags                = var.common_tags
+    tags                = var.common_tags
 }
 
 data "azurerm_key_vault_secret" "appinsights_secret" {

68e719a4f98aeab96d84f7b4d7e45f60c5ca7f25
damongreen123 <damon.green@btinternet.com>
2023-03-27 15:57:14 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,5 +10,4 @@ data "azurerm_key_vault" "div_key_vault" {
 data "azurerm_key_vault_secret" "appinsights_secret" {
   name = "AppInsightsInstrumentationKey"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
-  tags                = var.common_tags
 }

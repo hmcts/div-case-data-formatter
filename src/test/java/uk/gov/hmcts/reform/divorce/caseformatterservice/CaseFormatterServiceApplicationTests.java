9916ef430016e0615c5862968d178c3c402e0744
Mike Lemos <gobii@me.com>
2023-10-03 09:08:40 +0100

    DFR-2322 java 17 upgrade

--- src/test/java/uk/gov/hmcts/reform/divorce/caseformatterservice/CaseFormatterServiceApplicationTests.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/caseformatterservice/CaseFormatterServiceApplicationTests.java
@@ -2,14 +2,14 @@ package uk.gov.hmcts.reform.divorce.caseformatterservice;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.divorce.caseformatterservice.category.SmokeTest;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @Category(SmokeTest.class)
 @SpringBootTest
 public class CaseFormatterServiceApplicationTests {
